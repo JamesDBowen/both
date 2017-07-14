735e43f098944e9cf316a643a4bb6b0a84c0a23f
James <james@parthenonsoftware.com>
2017-07-13 23:02:04 -0600

    Fixed an issue where running redis after starting django caused the startup script to fail.

--- django/startup.sh
+++ django/startup.sh
@@ -1,7 +1,8 @@
+redis-server &
 python manage.py makemigrations
 python manage.py makemigrations both
 python manage.py migrate
-nohup python manage.py runserver 0.0.0.0:8000
-nohup python manage.py redis-server
-nohup python manage.py celery -A both beat -l info -S django
-nohup python manage.py celery worker -A both -l info
+nohup celery -A both beat -l info -S django &
+nohup celery worker -A both -l info &
+python manage.py runserver 0.0.0.0:8000
+
